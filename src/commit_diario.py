8ec485d8cfe8a1093e31532579230681d08c9a58
Pedro Emmanuel Buerger Da Silva <sombraios@hotmail.com>
2025-01-24 07:28:36 -0300

    normal

--- src/commit_diario.py
+++ src/commit_diario.py
@@ -41,9 +41,5 @@ def adicionar_e_enviar_arquivo():
     log_event(f"Arquivo {os.path.basename(FILENAME)} adicionado e enviado ao repositório!")
 
 if __name__ == "__main__":
-    dia_atual = datetime.datetime.now().weekday()
-    if dia_atual in [5, 6]:
-        log_event("Execução do script interrompida. Hoje é sábado ou domingo. Nenhuma ação foi realizada.")
-    else:
-        log_event("Execução do script iniciada.")
-        adicionar_e_enviar_arquivo()
+    log_event("Execução do script iniciada.")
+    adicionar_e_enviar_arquivo()
